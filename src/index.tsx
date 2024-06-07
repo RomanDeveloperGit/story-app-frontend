8f14c767dccac0bd5c4ac0eb75594bc9ea969a92
Roman <roman.developer@mail.ru>
2024-06-07 13:25:30 +0300

    chore(effector-logger): add logger for dev mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { attachLogger } from 'effector-logger';
 
 import { App } from './app';
+
+if (import.meta.env.DEV) {
+  attachLogger();
+}
 
 ReactDOM.createRoot(document.querySelector('#root')!).render(
   <React.StrictMode>
